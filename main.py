b35758f838ea188a7bddf4d3db3b21ca5429df12
joyceXxz <22025780+joyceXxz@users.noreply.github.com>
2019-03-14 01:00:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -100,12 +100,12 @@ def main():
     if args.cifar_type == 10:
         print('=> loading cifar10 data...')
         normalize = transforms.Normalize(mean=[0.491, 0.482, 0.447], std=[0.247, 0.243, 0.262])
-        shutil.copyfile("/data_public/~kriz/cifar-10-python.tar.gz", "data/cifar-10-python.tar.gz")
+        #shutil.copyfile("/data_public/~kriz/cifar-10-python.tar.gz", "data/cifar-10-python.tar.gz")
         
         train_dataset = torchvision.datasets.CIFAR10(
             root='./data', 
             train=True, 
-            download=True,
+            download=False,
             transform=transforms.Compose([
                 transforms.RandomCrop(32, padding=4),
                 transforms.RandomHorizontalFlip(),
@@ -117,7 +117,7 @@ def main():
         test_dataset = torchvision.datasets.CIFAR10(
             root='./data',
             train=False,
-            download=True,
+            download=False,
             transform=transforms.Compose([
                 transforms.ToTensor(),
                 normalize,
@@ -127,12 +127,12 @@ def main():
     else:
         print('=> loading cifar100 data...')
         normalize = transforms.Normalize(mean=[0.507, 0.487, 0.441], std=[0.267, 0.256, 0.276])
-        shutil.copyfile("/data_public/~kriz/cifar-100-python.tar.gz", "data/cifar-100-python.tar.gz")
+        #shutil.copyfile("/data_public/~kriz/cifar-100-python.tar.gz", "data/cifar-100-python.tar.gz")
         
         train_dataset = torchvision.datasets.CIFAR100(
             root='./data',
             train=True,
-            download=True,
+            download=False,
             transform=transforms.Compose([
                 transforms.RandomCrop(32, padding=4),
                 transforms.RandomHorizontalFlip(),
@@ -144,7 +144,7 @@ def main():
         test_dataset = torchvision.datasets.CIFAR100(
             root='./data',
             train=False,
-            download=True,
+            download=False,
             transform=transforms.Compose([
                 transforms.ToTensor(),
                 normalize,
